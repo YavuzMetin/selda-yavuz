6c1d07e5b2a67b107e1ba742dfe15709991030b6
YavuzMetin <yavuzmetin07@gmail.com>
2026-08-13 20:34:41 +0300

    Update love message

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './style.css'
 
 function App() {
-  return <h1>Hello World</h1>
+  return <h1>Seloşum seni çok seviyorum ❤️</h1>
 }
 
 createRoot(document.getElementById('root')).render(
